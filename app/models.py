97ff1943d3f9fbcba73635eb69f951ffb64fd967
Marcin <marcin.ubogi@gmail.com>
2025-01-26 16:07:26 +0100

    Add Paragon model and API endpoints for managing paragony

--- app/models.py
+++ app/models.py
@@ -4,3 +4,10 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     quantity = db.Column(db.Integer, default=0)
+
+class Paragon(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sklep = db.Column(db.String(128), nullable=False)
+    data = db.Column(db.Date, nullable=False)
+    laczna_cena = db.Column(db.Float, nullable=False)
+    rabat = db.Column(db.Float, default=0.0)
--- app/routes/paragony_bp.py
+++ app/routes/paragony_bp.py
@@ -1,7 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app.models import Paragon
 
 paragony_bp = Blueprint('paragony', __name__, url_prefix='/paragony')
 
 @paragony_bp.route('/')
 def index():
-    return "Paragony Blueprint"
+    paragony = Paragon.query.all()
+    paragony_list = []
+    for paragon in paragony:
+        paragony_list.append({
+            'id': paragon.id,
+            'sklep': paragon.sklep,
+            'data': paragon.data.isoformat(),
+            'laczna_cena': paragon.laczna_cena,
+            'rabat': paragon.rabat
+        })
+    return jsonify(paragony_list)
